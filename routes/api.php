def0d46ff0f4e72be950b30a0e8fd0f1ac375993
ianchang <ianchang@eztravel.com.tw>
2020-10-16 09:51:13 +0800

    feat: update routes for v8

--- routes/api.php
+++ routes/api.php
@@ -17,21 +17,17 @@ use Illuminate\Support\Facades\Route;
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
 // });
-Route::namespace('App\Http\Controllers')
-    ->group(function () {
-        // 註冊
-        Route::post('users', 'UserController@store')->name('signup');
-        // 登入
-        Route::post('tokens', 'TokenController@store')->name('login');
 
-        Route::middleware([
-            'auth:sanctum',
-        ])->group(function () {
-            Route::apiResource('todos', 'TodoController')->except('show');
-            // Route::match(['put', 'patch'],'todos', 'TodoController@update')->name('todos.update');
-            // Route::delete('todos', 'TodoController@destroy')->name('todos.destroy');
+// 註冊
+Route::post('users', [UserController::class, 'store'])->name('signup');
+// 登入
+Route::post('tokens', [TokenController::class, 'store'])->name('login');
 
-            // 登出
-            Route::delete('tokens', 'TokenController@destroy')->name('logout');
-        });
-    });
+Route::middleware([
+    'auth:sanctum',
+])->group(function () {
+    Route::apiResource('todos', TodoController::class)->except('show');
+
+    // 登出
+    Route::delete('tokens', [TokenController::class, 'destroy'])->name('logout');
+});
