b9be13e1ddff5cf33a06b0ff1de765f01ce2a366
ianchang <ianchang@eztravel.com.tw>
2020-10-16 14:11:51 +0800

    feat: update seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,14 +15,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        User::factory()->count(5)->has(
-            Todo::factory()
-                ->count(2)
-                ->state(function (array $attributes, User $user) {
-                    return ['created_by' => $user->id];
-                }))
-                ->create();
-        // User::factory(5)->hasTodos(2)->create();
+        // User::factory()->count(5)->has(
+        //     Todo::factory()
+        //         ->count(2)
+        //         ->state(function (array $attributes, User $user) {
+        //             return ['created_by' => $user->id];
+        //         }))
+        //         ->create();
+        User::factory(5)->hasTodos(2)->create();
         // \App\Models\User::factory(5)->create();
         // $this->call([
         //     TodoSeeder::class,
